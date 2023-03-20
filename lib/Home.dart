800b8174f2665794cba58528b830887a412f1130
Bunseang Tat <bunseangtat@Bunseangs-MacBook-Pro.local>
2023-03-20 11:11:19 +0700

    update from mac

--- lib/Home.dart
+++ lib/Home.dart
@@ -12,17 +12,16 @@ class Home extends StatefulWidget {
   State<Home> createState() => _HomeState();
 }
 
-class _HomeState extends State<Home>with SingleTickerProviderStateMixin {
-
+class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
   late TabController controller;
 
-  List pages=[
+  List pages = [
     Grid_Home.Grid_Home(),
     Grid_Acc.Grid_Acc(),
   ];
 
   int currentIndex = 0;
-  void onTap(int index){
+  void onTap(int index) {
     setState(() {
       currentIndex = index;
     });
@@ -31,34 +30,37 @@ class _HomeState extends State<Home>with SingleTickerProviderStateMixin {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
-      body: pages[currentIndex],
-      bottomNavigationBar:SizedBox(
-        height: 50,
-        child:  BottomNavigationBar(
-          backgroundColor: Colors.white,
-          elevation: 1,
-          onTap: onTap,
-          currentIndex: currentIndex,
-          selectedItemColor: Colors.indigo[900],
-          unselectedItemColor: Colors.grey,
-          selectedFontSize: 10,
-          unselectedFontSize: 10,
-          selectedLabelStyle: TextStyle(fontFamily: 'KhmerOSbattambang'),
-          unselectedLabelStyle: TextStyle(fontFamily: 'KhmerOSbattambang'),
-          items: [
-            BottomNavigationBarItem(
-              icon: Icon(Icons.home, size: 20,),
-              label: 'ទំព័រដើម'.tr,
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.account_box, size: 20,),
-              label: 'ចូលគណនី'.tr,
-            )
-          ],
-        ),
-      )
-    );
+        body: pages[currentIndex],
+        bottomNavigationBar: SizedBox(
+          height: 100,
+          child: BottomNavigationBar(
+            backgroundColor: Colors.white,
+            elevation: 1,
+            onTap: onTap,
+            currentIndex: currentIndex,
+            selectedItemColor: Colors.indigo[900],
+            unselectedItemColor: Colors.grey,
+            selectedFontSize: 10,
+            unselectedFontSize: 10,
+            selectedLabelStyle: TextStyle(fontFamily: 'KhmerOSbattambang'),
+            unselectedLabelStyle: TextStyle(fontFamily: 'KhmerOSbattambang'),
+            items: [
+              BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.home,
+                  size: 20,
+                ),
+                label: 'ទំព័រដើម'.tr,
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.account_box,
+                  size: 20,
+                ),
+                label: 'ចូលគណនី'.tr,
+              )
+            ],
+          ),
+        ));
   }
 }
-
