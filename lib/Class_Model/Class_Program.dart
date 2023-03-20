d804509a28b6eb5231b4d5c2bf1697dfa9444b37
SCT <vouchlanggm@gmail.com>
2023-03-20 11:23:56 +0700

    Update

--- lib/Class_Model/Class_Program.dart
+++ lib/Class_Model/Class_Program.dart
@@ -1,10 +1,30 @@
-class Program{
+class Program {
   late final String image;
   late final String fac_name;
 
   Program({required this.image, required this.fac_name});
 }
 
-List <Program> program = [
-  Program(image: 'image', fac_name: 'fac_name'),
+List<Program> program = [
+  Program(
+      image: 'assets/image/f-economic.png',
+      fac_name: 'មហាវិទ្យាល័យសេដ្ឋកិច្ច ពាណិជ្ជកម្មនិងទេសចរណ៍'),
+  Program(
+      image: 'assets/image/f-it.png',
+      fac_name: 'មហាវិទ្យាល័យវិទ្យាសាស្ត្រ និងបច្ចេកវិទ្យា'),
+  Program(
+      image: 'assets/image/f-law.png',
+      fac_name: 'មហាវិទ្យាល័យវិទ្យាសាស្ត្រសង្គម និងនីតិសាស្ត្រ'),
+  Program(
+      image: 'assets/image/f-art.png',
+      fac_name: 'មហាវិទ្យាល័យសិល្បៈ មនុស្សសាស្ត្រ និងអប់រំ'),
+  Program(
+      image: 'assets/image/fli.png',
+      fac_name: 'មហាវិទ្យាល័យអន្តរជាតិ'),
+  Program(
+      image: 'assets/image/f-internation-college.png',
+      fac_name: 'វិទ្យាស្ថានភាសាបរទេស'),
+  Program(
+      image: 'assets/image/post-graduate.png',
+      fac_name: 'សាលាថ្នាក់ក្រោយបរិញ្ញាបត្រ'),
 ];
