102dfff7e9b8d2421e8e702e6b9413d6f8540817
SCT <vouchlanggm@gmail.com>
2023-03-25 16:57:49 +0700

    Updaet

--- lib/Guest_Acc/Guest_Dashboard/VDO/Test_Vido.dart
+++ lib/Guest_Acc/Guest_Dashboard/VDO/Test_Vido.dart
@@ -2,33 +2,33 @@
 // import 'package:flutter_widget_from_html_core/flutter_widget_from_html_core.dart';
 // import 'package:fwfh_chewie/fwfh_chewie.dart';
 
-// class Video {
-//   final String id;
-//   final String title;
-//   final String thumbnailUrl;
-//   final String videoUrl;
+class Video {
+  final String id;
+  final String title;
+  final String thumbnailUrl;
+  final String videoUrl;
 
-//   Video({
-//     required this.id,
-//     required this.title,
-//     required this.thumbnailUrl,
-//     required this.videoUrl,
-//   });
-// }
+  Video({
+    required this.id,
+    required this.title,
+    required this.thumbnailUrl,
+    required this.videoUrl,
+  });
+}
 
-// class Playlist {
-//   final String id;
-//   final String title;
-//   final String description;
-//   final List<Video> videos;
+class Playlist {
+  final String id;
+  final String title;
+  final String description;
+  final List<Video> videos;
 
-//   Playlist({
-//     required this.id,
-//     required this.title,
-//     required this.description,
-//     required this.videos,
-//   });
-// }
+  Playlist({
+    required this.id,
+    required this.title,
+    required this.description,
+    required this.videos,
+  });
+}
 
 // class PlaylistListWidget extends StatelessWidget {
 //   final List<Playlist> playlists;
